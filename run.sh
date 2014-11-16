59ef819226a3375c775132d7cb28f586433491b6
Wang Yuan <junxue.w@gmail.com>
2014-11-16 13:33:10 +0800

    Added Xmx back.

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ git pull
 
 activator assembly
 
-java -Xms1024m -jar target/scala-2.11/DeepLearning-assembly-1.0.jar com.wangyz.Main &
+java -Xms1024m -Xmx3072m -jar target/scala-2.11/DeepLearning-assembly-1.0.jar com.wangyz.Main > out &
 
 
