d89425be9e972772778282041315f429533a605e
Wang Yuan <junxue.w@gmail.com>
2014-11-16 13:05:03 +0800

    tuning jvm options.

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ git pull
 
 activator assembly
 
-java -Xms2048m -Xmx3072m -XX:+CMSClassUnloadingEnabled -jar target/scala-2.11/DeepLearning-assembly-1.0.jar com.wangyz.Main &
+java -Xms1024m -jar target/scala-2.11/DeepLearning-assembly-1.0.jar com.wangyz.Main &
 
 
