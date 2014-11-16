2082a87b80b59f35ae7a0501490fc4900596e062
Wang Yuan <junxue.w@gmail.com>
2014-11-16 21:36:47 +0800

    Added more jam memory option.

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ git pull
 
 activator assembly
 
-java -Xms1024m -Xmx3072m -jar target/scala-2.11/DeepLearning-assembly-1.0.jar com.wangyz.Main > out &
+java -Xms2048m -Xmx4096m -XX:+CMSClassUnloadingEnabled -jar target/scala-2.11/DeepLearning-assembly-1.0.jar com.wangyz.Main > out &
 
 
